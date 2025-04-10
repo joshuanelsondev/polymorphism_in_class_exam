8d0ede2a12b3edc82018df3c2074f1bae9aa044f
joshuanelsondev <joshuanelsondev@gmail.com>
2025-04-09 21:34:50 -0400

    feat: implement Notification class and methods

--- src/main/java/org/example/Notification.java
+++ src/main/java/org/example/Notification.java
@@ -1,5 +1,15 @@
 package org.example;
 
 public abstract class Notification {
+    public String recipient;
+    public String message;
+
+    public Notification(String recipient, String message) {
+       this.recipient = recipient;
+       this.message = message;
+    }
+
+    public abstract void send(String message);
+
 
 }
