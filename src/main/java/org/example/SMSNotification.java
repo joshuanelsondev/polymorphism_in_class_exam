5370e61459183c4108bbf29c0f7c03d9a2e155a9
joshuanelsondev <joshuanelsondev@gmail.com>
2025-04-09 22:05:07 -0400

    feat: implement logic in SMSNotification class

--- src/main/java/org/example/SMSNotification.java
+++ src/main/java/org/example/SMSNotification.java
@@ -1,5 +1,17 @@
 package org.example;
 
 public class SMSNotification extends Notification {
- 
+
+    public SMSNotification(String recipient, String message) {
+        super(recipient, message);
+    }
+
+    @Override
+    public void send(String recipient, String message) {
+        System.out.println("Sending to: +1234567890");
+        System.out.println("SMS content: Test SMS");
+        System.out.println("Connecting to SMS gateway...");
+        System.out.println("Sending SMS...");
+    }
+
 }
