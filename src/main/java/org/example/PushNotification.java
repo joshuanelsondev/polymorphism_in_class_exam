c9339b19737eb6bd0e20e42cd932f790b14d691f
joshuanelsondev <joshuanelsondev@gmail.com>
2025-04-09 22:08:45 -0400

    feat: handle logic for PushNotification class

--- src/main/java/org/example/PushNotification.java
+++ src/main/java/org/example/PushNotification.java
@@ -2,4 +2,15 @@ package org.example;
 
 public class PushNotification extends Notification {
 
+    public PushNotification(String recipient, String message) {
+        super(recipient, message);
+    }
+
+    @Override
+    public void send(String recipient, String message) {
+        System.out.println("Sending to: device123");
+        System.out.println("Push notification content: Test Push");
+        System.out.println("Connecting to push notification service...");
+        System.out.println("Sending push notification...");
+    }
 }
